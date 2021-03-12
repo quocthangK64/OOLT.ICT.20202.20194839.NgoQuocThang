4e722ed79c4c0ad7b86c8091eb92a651ec5d1214
quocthangK64 <thang.nq194839@sis.hust.edu.vn>
2021-03-12 13:56:54 +0700

    push work 13h56

--- Lab03/AimsProject/src/DigitalVideoDisc.java
+++ Lab03/AimsProject/src/DigitalVideoDisc.java
@@ -1,4 +1,56 @@
 
 public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    //Question in part 5. Create Constructor method
+    //Answer: Yes. JAVA allow us to do this. It's called overloading method
+    // 2 constructor are different from variables (title and category)
+	public DigitalVideoDisc(String title) {
+		super();
+		this.title = title;
+	}
+	
+	public DigitalVideoDisc(String title, String category, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+	}
+    
+	public DigitalVideoDisc(String title, String category, String director, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.cost = cost;
+	}
+    
+	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+	}
 
+	public String getTitle() {
+		return title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
+    
 }
