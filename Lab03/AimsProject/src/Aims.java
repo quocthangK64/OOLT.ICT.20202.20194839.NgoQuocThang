5e977fc6bca8456886a29e1cd6bcc016930e9df5
quocthangK64 <thang.nq194839@sis.hust.edu.vn>
2021-03-13 14:55:36 +0700

    update 14h55 Sat

--- Lab03/AimsProject/src/Aims.java
+++ Lab03/AimsProject/src/Aims.java
@@ -1,9 +1,27 @@
+import javax.swing.JOptionPane;
 
 public class Aims {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Cart anOrder = new Cart();
+		
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+				"Animation","Roger Allers", 87, 19.95f);
+		
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Stars Wars",
+				"Science Fiction","George Lucas", 87, 24.95f);
+		
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
+				"Animation", 18.99f);
+		// check totalCost and removeDVD
+		anOrder.addDigitalVideoDisc(dvd1);
+		anOrder.addDigitalVideoDisc(dvd2);
+		anOrder.removeDigitalVideoDisc(dvd1);
+		anOrder.removeDigitalVideoDisc(dvd2);
+		anOrder.removeDigitalVideoDisc(dvd3);
+		// check addDVD full. Set MAX_NUMBERS_ORDERED = 3 in Cart class
+		JOptionPane.showMessageDialog(null, "Total Cost is: " + anOrder.totalCost());
+		
 	}
-
 }
