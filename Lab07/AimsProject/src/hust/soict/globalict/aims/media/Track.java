1a344e266a2b8c44932ecd737b076b7aea54df67
quocthangK64 <thang.nq194839@sis.hust.edu.vn>
2021-04-09 22:19:26 +0700

    create Track class

---
+++ Lab07/AimsProject/src/hust/soict/globalict/aims/media/Track.java
@@ -0,0 +1,23 @@
+package hust.soict.globalict.aims.media;
+
+public class Track {
+	private String title;
+	private int length;
+	public Track(String title) {
+		this.title = title;
+	}
+	public Track(int length) {
+		this.length = length;
+	}
+	public Track(String title, int length) {
+		super();
+		this.title = title;
+		this.length = length;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public int getLength() {
+		return length;
+	}
+}
