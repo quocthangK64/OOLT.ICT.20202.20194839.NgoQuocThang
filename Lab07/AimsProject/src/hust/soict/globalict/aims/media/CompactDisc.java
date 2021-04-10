d0ba943d237d10fe2bfc5cf76b31a0c48ee605c1
quocthangK64 <thang.nq194839@sis.hust.edu.vn>
2021-04-10 14:17:51 +0700

    update CompactDisc class

--- Lab07/AimsProject/src/hust/soict/globalict/aims/media/CompactDisc.java
+++ Lab07/AimsProject/src/hust/soict/globalict/aims/media/CompactDisc.java
@@ -1,9 +1,43 @@
 package hust.soict.globalict.aims.media;
 
-public class CompactDisc extends Disc{
+import java.util.ArrayList;
 
+public class CompactDisc extends Disc{
+	private String artist;
+	private ArrayList<Track> tracks = new ArrayList<Track>();
+	
 	public CompactDisc() {
 		// TODO Auto-generated constructor stub
 	}
-
+	public CompactDisc(String title, String category, float cost) {
+		super(title,category,cost);
+	}
+	public CompactDisc(String title) {
+		super(title);
+	}
+	
+	public String getArtist() {
+		return artist;
+	}
+	public void addTrack(Track t) {
+		if(tracks.contains(t)) {
+			System.out.println("The input track is already in the list of tracks.");
+		}else {
+			tracks.add(t);
+		}
+	}
+	public void removeTrack(Track t) {
+		if(tracks.contains(t)) {
+			tracks.remove(t);
+		}else {
+			System.out.println("The input track had not been in the list of tracks.");
+		}
+	}
+	public int getLength() {
+		int cdlength = 0;
+		for(int i = 0; i < tracks.size(); i++ ) {
+			cdlength += tracks.get(i).getLength();
+		}
+		return cdlength;
+	}
 }
